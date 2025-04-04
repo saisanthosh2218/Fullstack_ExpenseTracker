fc7b501b2039b1b8e05d70b2b472a692eef1cab3
saisanthosh2218 <saisanthoshbunni@gmail.com>
2025-04-04 22:56:39 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -13,10 +13,16 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000',process.env.FRONTEND_URL],
+  origin: [
+    'http://localhost:3000',
+    process.env.FRONTEND_URL
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token']
 }));
+
+// Middleware
 app.use(express.json());
 
 // Connect to MongoDB
